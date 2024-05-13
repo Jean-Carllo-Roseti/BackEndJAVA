63346ccf1599dbf21724af44ca35028f6b8cc50d
jean carllo <jeanrosete04@gmail.com>
2024-05-13 15:24:22 -0300

    entrega da tarefa do módulo 8.

---
+++ TarefaMod8/src/TarefaMod8.java
@@ -0,0 +1,17 @@
+public class TarefaMod8 {
+    public static void main ( String args []) {
+        Media();
+    }
+
+    private static void Media() {
+         int nota1 = 3;
+         int nota2 = 3;
+         int nota3 = 3;
+         int nota4 = 3;
+
+         int media = (nota4 + nota2 + nota3+ nota4) / 4;
+
+
+        System.out.println("a média é a seguinte é " + media);
+    }
+}
