74750a74067939a73a96eeac24f92ff3639ceff2
jean carllo <jeanrosete04@gmail.com>
2024-07-02 18:11:51 -0300

    acredito ter finalizado.

---
+++ TarefaMOD25/src/test/java/br/com/jcmar/AllTests.java
@@ -0,0 +1,10 @@
+package br.com.jcmar;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ ClienteServiceTest.class, ClienteDAOTest.class})
+public class AllTests {
+
+}
