1a9c09f8f777902e8765e6b3f1cdcab94a8aa1d9
jean carllo <jeanrosete04@gmail.com>
2024-07-03 10:45:25 -0300

    ainda com problema na class: connetionFactory.

--- pro3/src/main/java/br/com/jcmar/dao/generic/jdbc/dao/IClienteDAO.java
+++ pro3/src/main/java/br/com/jcmar/dao/generic/jdbc/dao/IClienteDAO.java
@@ -6,13 +6,10 @@ import java.util.List;
 
 public interface IClienteDAO {
 
+
     public Integer cadastrar(Cliente cliente) throws Exception;
 
-    public Integer atualizar(Cliente cliente) throws Exception;
+    public Cliente consultar(String codigo) throws Exception;
 
-    public Cliente buscar(String codigo) throws Exception;
-
-    public List<Cliente> buscarTodos() throws Exception;
-
-    public Integer excluir(Cliente cliente) throws Exception;
+    public Integer excluir(Cliente clienteBD) throws Exception;
 }
